d126f5601adc70531fca22ecdcb22b9919497e0a
jungheol <jungheol@naver.com>
2023-12-25 16:00:31 +0900

    Box 스크립트 작성

---
+++ SimpleGames/Assets/Scripts/BubbleGame/Box.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Box : MonoBehaviour {
+
+    public Sprite[] boxSpirtes;
+
+    void Start() {
+        float x = Random.Range(-2.5f, 2.5f);
+        float y = Random.Range(4.0f, 5.0f);
+
+        transform.position = new Vector3(x, y, 0);
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = boxSpirtes[Random.Range(0, 6)];
+        float size = Random.Range(2.5f, 5.5f);
+        transform.localScale = new Vector3(size, size, 1);
+    }
+
+    void Update() {
+        if(transform.position.y < -5.0f) Destroy(gameObject); 
+    }
+}
