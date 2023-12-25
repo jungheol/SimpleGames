70c45a434bf15c5a346eef38fbf86c969b79d1c3
jungheol <jungheol@naver.com>
2023-12-25 16:20:07 +0900

    TopManager 스크립트 생성

---
+++ SimpleGames/Assets/Scripts/Manager/TopManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopManager : MonoBehaviour {
+    public enum SceneName {
+        INTROSCENE = 0,
+        BOXDEFENSE,
+        DOGVSCAT,
+        FINDCARD,
+        RAINDROP,
+    }
+}
