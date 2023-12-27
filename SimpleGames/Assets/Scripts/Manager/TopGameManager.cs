09f57ed3c25f8c6467905a5eda2dea25eaea0742
jungheol <jungheol@naver.com>
2023-12-27 20:38:36 +0900

    상속을 위해서 TopGameManager 생성

---
+++ SimpleGames/Assets/Scripts/Manager/TopGameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopGameManager : MonoBehaviour {
+	
+	protected virtual string gameSceneName => "";
+	protected internal virtual void RetryGame() {
+		
+	}
+}
